85eb4e465bd63cbe4a26f2c24aebf07cd2658b08
Fred Ekstrand <fredekstrandgithub@gmail.com>
2017-02-22 19:28:24 -0500

    beer

---
+++ HttpWebRequestExtension/HttpWebRequestExtension.cs
@@ -0,0 +1,98 @@
+﻿/*
+Open source MIT License
+Copyright(c) 2017 Fred A Ekstrand Jr.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+8Y95-1833S-L669M-H0PD9-3333W-2313E-U9D7
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Reflection;
+
+namespace Ekstrand.Net
+{
+    /// <summary>
+    /// Allow for HTTP web request header section that are set by the system or set by properties or methods to be directly edited.
+    /// </summary>
+    /// <remarks>Source from: http://stackoverflow.com/questions/239725/cannot-set-some-http-headers-when-using-system-net-webrequest</remarks>
+    public static class HttpWebRequestExtension
+    {
+        
+        static string[] RestrictedHeaders = new string[]
+        {
+            "Accept",
+            "Connection",
+            "Content-Length",
+            "Content-Type",
+            "Date",
+            "Expect",
+            "Host",
+            "If-Modified-Since",
+            "Keep-Alive",
+            "Proxy-Connection",
+            "Range",
+            "Referer",
+            "Transfer-Encoding",
+            "User-Agent"
+        };
+
+        static Dictionary<string, PropertyInfo> HeaderProperties = new Dictionary<string, PropertyInfo>(StringComparer.OrdinalIgnoreCase);
+
+
+        static HttpWebRequestExtension()
+        {
+            Type type = typeof(HttpWebRequest);
+            foreach (string header in RestrictedHeaders)
+            {
+                string propertyName = header.Replace("-", "");
+                PropertyInfo headerProperty = type.GetProperty(propertyName);
+                HeaderProperties[header] = headerProperty;
+            }
+        }
+
+        /// <summary>
+        /// Set the HTTP web request header values including restricted access headers values.
+        /// </summary>
+        /// <param name="request">WebRequest</param>
+        /// <param name="name">Header name item</param>
+        /// <param name="value">Header item value</param>
+        public static void SetRawHeader(this HttpWebRequest request, string name, string value)
+        {
+            if (HeaderProperties.ContainsKey(name))
+            {
+                PropertyInfo property = HeaderProperties[name];
+                if (property.PropertyType == typeof(DateTime))
+                    property.SetValue(request, DateTime.Parse(value), null);
+                else if (property.PropertyType == typeof(bool))
+                    property.SetValue(request, Boolean.Parse(value), null);
+                else if (property.PropertyType == typeof(long))
+                    property.SetValue(request, Int64.Parse(value), null);
+                else
+                    property.SetValue(request, value, null);
+            }
+            else
+            {
+                request.Headers[name] = value;
+            }
+        }
+    }
+}
---
+++ HttpWebRequestExtension/Properties/AssemblyInfo.cs
@@ -0,0 +1,81 @@
+﻿/*
+Open source MIT License
+Copyright(c) 2017 Fred A Ekstrand Jr.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+8Y95-1833S-L669M-H0PD9-3333W-2313E-U9D7
+*/
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HttpWebRequestExtension")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HttpWebRequestExtension")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8ad5884a-c02f-47b5-a565-704af1b99c3b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyKey("8Y95-1833S-L669M-H0PD9-3333W-2313E-U9D7")]
+
+#pragma warning disable CS1591
+[AttributeUsage(AttributeTargets.Assembly)]
+public class AssemblyKey : Attribute
+{
+    private string m_KeyValue;
+
+    public AssemblyKey(string value)
+    {
+        this.m_KeyValue = value;
+    }
+
+    public string Key
+    {
+        get { return m_KeyValue; }
+    }
+}
+#pragma warning restore CS1591
